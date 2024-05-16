e4ae3cc012aa5094c057043f98e809737af8893c
Shamim <s.ahsan.ds96@gmail.com>
2024-05-16 10:43:37 +0600

    task model updated - timestamps added

--- models/Task.js
+++ models/Task.js
@@ -1,21 +1,20 @@
 import mongoose from "mongoose";
 
-const TaskSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "must provide name"],
-    trim: true,
-    maxlength: [20, "name can not be more than 20 characters"],
+const TaskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "must provide name"],
+      trim: true,
+      maxlength: [20, "name can not be more than 20 characters"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  { timestamps: true }
+);
 
 const Task = mongoose.model("Task", TaskSchema);
 
